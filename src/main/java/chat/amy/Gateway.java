72a0e3e5623c39250fc846c2bfd28170f3c8ec27
Amy Null <amy@willnot.date>
2017-09-17 11:23:56 -0400

    Initial rewrite commit

---
+++ src/main/java/chat/amy/Gateway.java
@@ -0,0 +1,15 @@
+package chat.amy;
+
+/**
+ * @author amy
+ * @since 9/17/17.
+ */
+public final class Gateway {
+    private Gateway() {
+    
+    }
+    
+    public static void main(final String[] args) {
+    
+    }
+}
