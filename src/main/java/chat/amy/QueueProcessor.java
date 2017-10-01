b090a7f308a9294c0e46c29c4b5524bf2ae9d618
Amy Null <amy@willnot.date>
2017-10-01 00:01:25 -0400

    Migrate from Redisson

--- src/main/java/chat/amy/QueueProcessor.java
+++ src/main/java/chat/amy/QueueProcessor.java
@@ -1,65 +1,74 @@
 package chat.amy;
 
-import org.redisson.Redisson;
-import org.redisson.api.RBlockingQueue;
-import org.redisson.api.RFuture;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * @author amy
  * @since 9/22/17.
  */
+@SuppressWarnings("unused")
 public class QueueProcessor implements Runnable {
+    @SuppressWarnings("FieldCanBeLocal")
     private final Gateway gateway;
     private final String queue;
-    private final RedissonClient redis;
+    private final String outputQueue;
+    private final JedisPool redis;
+    
     private final Logger logger;
     
+    @SuppressWarnings("WeakerAccess")
     public QueueProcessor(final Gateway gateway, final String queue, final int idx) {
         this.gateway = gateway;
         this.queue = queue;
+        outputQueue = queue.replace("intake", "backend");
         logger = LoggerFactory.getLogger("Gateway " + queue + " Processor " + idx);
         
-        final Config config = new Config();
-        config.useSingleServer().setAddress(Optional.ofNullable(System.getenv("REDIS_HOST")).orElse("redis://redis:6379"))
-                .setPassword(System.getenv("REDIS_PASS"))
-                // Based on my bot heavily abusing redis as it is, high connection pool size is not a terrible idea.
-                // NOTE: Current live implementation uses like 500 connections in the pool, so TEST TEST TEST
-                // TODO: Determine better sizing
-                .setConnectionPoolSize(128);
-        redis = Redisson.create(config);
+        final JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(1024);
+        jedisPoolConfig.setMaxTotal(1024);
+        jedisPoolConfig.setMaxWaitMillis(500);
+        redis = new JedisPool(jedisPoolConfig, Optional.ofNullable(System.getenv("REDIS_HOST")).orElse("redis://redis:6379"));
+    }
+    
+    private void cache(final Consumer<Jedis> op) {
+        try(Jedis jedis = redis.getResource()) {
+            jedis.auth(System.getenv("REDIS_PASS"));
+            op.accept(jedis);
+        }
     }
     
     @Override
     public void run() {
+        //noinspection InfiniteLoopStatement
         while(true) {
-            try {
-                logger.debug("Getting next event from " + queue + "...");
-                final RBlockingQueue<WrappedEvent> blockingQueue = redis.getBlockingQueue(queue);
-                final RFuture<WrappedEvent> future = blockingQueue.takeAsync();
-                final WrappedEvent event = future.await().getNow();
-                if(future.isDone()) {
-                    logger.debug("Future was a success!");
-                    logger.debug("Cause: " + future.cause());
+            logger.debug("Getting next event from " + queue + "...");
+            cache(jedis -> {
+                // Just block forever
+                final List<String> blpop = jedis.blpop(0, queue);
+                // [0] is key, [1] is value
+                // sanity check
+                if(blpop.get(0).equalsIgnoreCase(queue)) {
+                    final String event = blpop.get(1);
+                    // Any processing etc. goes here
+                    logger.debug("Got next event: " + event);
+                    if(Objects.nonNull(event)) {
+                        jedis.rpush(outputQueue, event);
+                    } else {
+                        logger.debug("Ignoring null queue event");
+                    }
                 } else {
-                    logger.debug("Somehow got the future without it being done!?");
+                    logger.warn("Somehow didn't pop from " + queue + "!?");
                 }
-                // Do processing etc. here
-                logger.debug("Got next event: " + event);
-                if(Objects.nonNull(event)) {
-                    redis.getBlockingQueue(queue.replace("intake", "backend")).add(event);
-                } else {
-                    logger.debug("Ignoring null queue event");
-                }
-            } catch(final InterruptedException e) {
-                e.printStackTrace();
-            }
+            });
         }
     }
 }
